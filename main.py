2bc41c6b2b27b1cc1b3d656ea7cc1ed22fb9ae2e
gilvanh4o-cell <gilvan.h4o@gmail.com>
2026-03-12 16:26:52 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import os
-
-# Conteúdo da sua lista IPTV
-conteudo = "#EXTM3U\n#EXTINF:-1, Canal de Teste Gilvan\nhttp://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-
-# Criando o arquivo da lista
+conteudo = "#EXTM3U\n#EXTINF:-1, Canal de Teste Gilvan\nhttp://link.com"
 with open("lista_gilvan.m3u", "w", encoding="utf-8") as f:
     f.write(conteudo)
-
-print("Arquivo lista_gilvan.m3u criado com sucesso!")
+print("Sucesso!")
