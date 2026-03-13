b34b91902e2130c89abae2c28892fae06bf1e5da
gilvanh4o-cell <gilvan.h4o@gmail.com>
2026-03-13 09:03:35 -0300

    Excluir main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-conteudo = "#EXTM3U\n#EXTINF:-1, Canal de Teste Gilvan\nhttp://link.com"
-with open("lista_gilvan.m3u", "w", encoding="utf-8") as f:
-    f.write(conteudo)
-print("Sucesso!")
